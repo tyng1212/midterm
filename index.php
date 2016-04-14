81b6d2c07a1c1562670e5ca98c7be67788089d12
tyng1212 <tyng1212@users.noreply.github.com>
2016-04-14 12:07:43 +0800

    Added files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head><title>Login Success</title>
+</head>
+<body>
+
+<form action="" method="POST">
+echo $_POST["$uName"];
+歡迎回來！<br/>
+登入次數:<br/>
+您的上一次登入時間為:<br/>
+</form>
+</body>
+</html>
